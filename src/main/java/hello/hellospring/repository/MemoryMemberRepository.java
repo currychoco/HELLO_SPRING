58bda0270b0fa3d5c78c525909f4eb073c38e42a
currychoco <godqhrgka0000@naver.com>
2022-07-11 21:40:10 +0900

    java Configuration으로 Bean생성

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import hello.hellospring.domain.Member;
 
-@Repository
+
 public class MemoryMemberRepository implements MemberRepository{
 
 	private static Map<Long, Member> store = new HashMap<Long, Member>();
